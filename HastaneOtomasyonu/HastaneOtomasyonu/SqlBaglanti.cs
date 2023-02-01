20fd986e47e93476483ebb21ffa54939890255e9
sudenuurr <118157578+sudenuurr@users.noreply.github.com>
2023-02-01 19:46:20 +0300

    HastaneOtomasyon
    C# windows form ile hastane otomasyon projesi

---
+++ HastaneOtomasyonu/HastaneOtomasyonu/SqlBaglanti.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+
+namespace HastaneOtomasyonu
+{
+    public class SqlBaglanti
+    {
+        public SqlConnection baglanti()
+        {
+
+            SqlConnection baglan = new SqlConnection("Data Source=LAPTOP-S9BLVBOD;Initial Catalog=HastaneOtomasyon;Integrated Security=True");
+            baglan.Open();
+            return baglan;
+
+        }
+    }
+}
